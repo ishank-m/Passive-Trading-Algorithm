64493653fef3663e2e644d77254708bb3c09cbab
Ishank <ishankmishra19@gmail.com>
2025-04-06 00:44:29 +0530

    Adding dynamically updating price range

--- Main.py
+++ Main.py
@@ -1,5 +1,22 @@
+import yfinance as yf, time
+
 
 state: str = None       #current state of buy/sell/hold of the stock
 range: list = []        #range=[min,max]
 target_low: int = None
 target_high: int = None
+
+
+def dynamic_range():
+    global range
+    while True:
+        ticker = yf.Ticker("NVDA")
+        data = ticker.history(period = "1d", interval = "1m")
+        current_price = float(data["Close"].iloc[-1])
+        range = [current_price-1, current_price+1]
+        print(range)
+        time.sleep(60)
+
+dynamic_range()
+
+
